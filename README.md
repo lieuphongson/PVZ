6249a79b95f8a6e438be037155d36f0173b8c54a
lieuphongson <royaanh@gmail.com>
2020-11-28 13:50:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+It's about Plants VS Zombies
+Enjoy the game!!
+
+Contributors to complete and update the product:
++ Liêu Phong Sơn- ITITIU18200
++ Bùi Trung Kiên- ITITIU18201
+Contact via Facebook:
++ Liêu Phong Sơn| https://www.facebook.com/lieuphongson.O17
++ Bùi Trung Kiên| https://www.facebook.com/kien.bui.23/
+
+
---
+++ src/gui/DebugTool.java
@@ -0,0 +1,23 @@
+package gui;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+import com.Controller;
+
+
+public class DebugTool {
+	
+	/**
+	 * Draw cursor coordinate
+	 * @param g Graphics get from render process
+	 * @throws SlickException
+	 */
+	public static void showMousePosition(Graphics g) throws SlickException {
+		if (Mouse.getX() < PZGUI.getWidth() - 100)
+			g.drawString( String.format("[%d, %d]", Controller.getMouseX(), Controller.getMouseY()) , Controller.getMouseX() + 10, Controller.getMouseY());
+		else
+			g.drawString( String.format("[%d, %d]", Controller.getMouseX(), Controller.getMouseY()), Controller.getMouseX() - 100, Controller.getMouseY());
+	}
+}
